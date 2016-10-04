7e919c30db6444e71690cb43f4149ca2967ddd1f
František Brožka <f.brozka@gmail.com>
2016-10-04 12:29:10 +0200

    first python code tryout

---
+++ main.py
@@ -0,0 +1,33 @@
+
+
+
+import pygame
+
+
+
+
+class Picture:
+  def __init__(self,path,layer):
+    self.path=path
+    self.picture=self.load(path)
+    self.pos=[0,0]
+    self.layer=layer
+  def load(self,path):
+    return picture
+
+
+class ActionsTriggeredByUser:
+  def __init__(self):
+    pass
+  def userTriggeredProcessOpenImageFile(self, game):
+    path=PathGetter(path='',mode=1,caption='Select Picture File To Load')
+    self.tryToLoadImage(path)
+  def userTriggeredProcessCloneImage(self, game):
+    pass
+  def userTriggeredProcessChangeLayerOfPicture(self):
+    ...
+    if :
+       
+  def userTriggeredProcess
+
+class GUI:
