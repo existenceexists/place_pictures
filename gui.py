dbb15bb09d9b6c7888def54ae8b454fa6a841d90
František Brožka <sentientfanda@gmail.com>
2017-12-06 12:28:14 +0100

    Remake displaying dialog window to open picture file using FunnyGui
    Because of that remake updating and drawing of GUI widgets
    as handling FunnyGUI widgets is different from handling MenuSystem widgets.
    
    It is buggy:
    
    Newline in Label instance is not making newline,
    this will not change in FunnyGUI, we have to deal with it another way.
    
    FunnyGUI widgets contained in the Window widget instance
    do not react on mouse hovering over it,
    it is caused by a bug in FunnyGUI regarding positions of mouse events,
    FunnyGUI git commit bcbb9afc4bbc32bae68881471459752921b80bbb.

--- gui.py
+++ gui.py
@@ -12,12 +12,11 @@ class Gui:
   def __init__(self,game):
     
     self.game=game
-    self.widgets_container=[]
-    self.widgets_to_draw_basic=[]
-    self.widgets_to_draw=[]
+    self.container_widgets_FunnyGUI=[]
+    self.widgets_MenuSystem_to_draw_basic=[]
+    self.widgets_MenuSystem_to_draw=[]
     self.create_gui()
     self.path_picture_to_open=None
-    self.sgc_dialog_window_shown=False
 
   def create_gui(self):
     # based on exemple.py in MenuSystem package, the comments are in French
@@ -40,11 +39,10 @@ class Gui:
     self.menu_select = MenuSystem.MenuSystem.Menu('select',('same file and zoom','same file','layers','all on screen','all'))
     
     #~ création de la barre
-    self.bar = MenuSystem.MenuSystem.MenuBar()
-    self.widgets_container.append(self.bar)
-    menu_bar_rect=self.bar.set((self.menu_game,self.menu_map,self.menu_layer,self.menu_picture,self.menu_selection,self.menu_select))
-    self.widgets_to_draw_basic.append([self.game.screen.subsurface(menu_bar_rect).copy(),menu_bar_rect])
-    self.widgets_to_draw=list(self.widgets_to_draw_basic)
+    self.menu_bar=MenuSystem.MenuSystem.MenuBar()
+    menu_bar_rect=self.menu_bar.set((self.menu_game,self.menu_map,self.menu_layer,self.menu_picture,self.menu_selection,self.menu_select))
+    self.widgets_MenuSystem_to_draw_basic.append([self.game.screen.subsurface(menu_bar_rect).copy(),menu_bar_rect])
+    self.widgets_MenuSystem_to_draw=list(self.widgets_MenuSystem_to_draw_basic)
     
     #self.label_selected_pictures_count=label.Label('pictures:  0',(200,200,200,255),(80,80,80,80))
     #self.label_selected_pictures_count.set_topright_position((990,60))
@@ -54,55 +52,59 @@ class Gui:
     #self.widgets_container.append(self.label_selected_picture_types_count)
     
   def create_dialog_open_picture_file(self):
-    label_title = sgc.Label(text="""Open and show picture file.""",col=(150,150,150))
-    label_title.pos=(0,0)
-    label_zoom=sgc.Label(text="""Enter zoom percent. \nThe size of the new picture will be scaled \nto the given percent size of the original picture. \nThe number can be an integer or float number between 0 and infinity.""",col=(150,150,150))
-    label_zoom.pos=(0,50)
-    self.input_box_zoom = sgc.InputBox(label="zoom percent",default="100")
-    self.input_box_zoom.pos = (0,140)
-    self.input_box_zoom.text = "100"
+    self.window_dialog_open_picture_file=FunnyGUI.window.Window()
+    self.window_dialog_open_picture_file.rect.move_ip(100,100)
+    self.container_widgets_FunnyGUI.append(self.window_dialog_open_picture_file)
+    label_title=FunnyGUI.label.Label(text="""Open and show picture file.""")
+    label_title.rect.move_ip(50,50)
+    self.window_dialog_open_picture_file.add(label_title)
+    label_zoom=FunnyGUI.label.Label(text="""Enter zoom percent. \nThe size of the new picture will be scaled \nto the given percent size of the original picture. \nThe number can be an integer or float number between 0 and infinity.""")
+    label_zoom.rect.move_ip(50,80)
+    self.window_dialog_open_picture_file.add(label_zoom)
+    self.input_box_zoom=FunnyGUI.inputbox.InputBox()
+    self.input_box_zoom.SetText("100")
+    self.input_box_zoom.rect.move_ip(50,200)
+    self.window_dialog_open_picture_file.add(self.input_box_zoom)
     top_layer=self.game.pictures.get_number_of_layers()
     if top_layer==0:
       top_layer=1
-    label_layer=sgc.Label(text="""Enter layer number. \nThe new picture will be moved into the layer. \nThe number can be an integer number between 1 and """+str(top_layer)+""" .""",col=(150,150,150))
-    label_layer.pos=(0,190)
-    self.input_box_layer = sgc.InputBox(label="layer number",default="1")
-    self.input_box_layer.pos = (0,270)
-    self.input_box_layer.text = "1"
-    btn_ok = sgc.Button(label="OK", pos=(100,320))
-    btn_ok.on_click = self.confirm_dialog_open_picture_file
-    container_dialog_open_picture_file=sgc.Container(widgets=(label_title,label_zoom,self.input_box_zoom,label_layer,self.input_box_layer,btn_ok), border=10)
-    # Display dialog window, on_click replaced through inheritance
-    self.dialog_open_picture_file=sgc.Dialog(widget=container_dialog_open_picture_file,title="Open picture file")
-    self.dialog_open_picture_file.rect.center = self.game.screen.rect.center
-    self.sgc_dialog_window_shown
+    label_layer=FunnyGUI.label.Label(text="""Enter layer number. \nThe new picture will be moved into the layer. \nThe number can be an integer number between 1 and """+str(top_layer)+""" .""")
+    label_layer.rect.move_ip(50,230)
+    self.window_dialog_open_picture_file.add(label_layer)
+    self.input_box_layer=FunnyGUI.inputbox.InputBox()
+    self.input_box_layer.SetText("1")
+    self.input_box_layer.rect.move_ip(50,250)
+    self.window_dialog_open_picture_file.add(self.input_box_layer)
+    btn_ok=FunnyGUI.button.Button(text="OK",onClickCallback=self.confirm_dialog_open_picture_file)
+    btn_ok.rect.move_ip(50,280)
+    self.window_dialog_open_picture_file.add(btn_ok)
     
   def update(self, event):
-    
+    return_value=False
     self.game.pictures.do_not_interact_with_pictures=False
-    rect_list=[]
-    for widget in self.widgets_container:
-      rect_list_from_update=widget.update(event)
-      if rect_list_from_update:
-        rect_list.extend(rect_list_from_update)
-        self.game.pictures.do_not_interact_with_pictures=True
-        if widget==self.bar:
-          if self.bar.choice:
-            if self.bar.choice_index==(0,3):
-              self.game.exit()
-            
-            elif self.bar.choice_index==(3,0):
-              self.show_dialog_open_picture_file()
+    rect_list=self.menu_bar.update(event)
     if rect_list:
-      self.widgets_to_draw=list(self.widgets_to_draw_basic)
+      # Menu bar changed it's image because user interacted with it.
+      return_value=True
+      self.game.pictures.do_not_interact_with_pictures=True
+      self.widgets_MenuSystem_to_draw=list(self.widgets_MenuSystem_to_draw_basic)
       for rect in rect_list:
-        self.widgets_to_draw.append([self.game.screen.subsurface(rect).copy(), rect])
-      return True
-    return False
+        self.widgets_MenuSystem_to_draw.append([self.game.screen.subsurface(rect).copy(),rect])
+      if self.menu_bar.choice:
+        if self.menu_bar.choice_index==(0,3):
+          self.game.exit()
+        elif self.menu_bar.choice_index==(3,0):
+          self.show_dialog_open_picture_file()
+    for widget in self.container_widgets_FunnyGUI:
+      if widget.update(event):
+        return_value=True
+    return return_value
   
   def draw(self):
-    for widget_and_rect in self.widgets_to_draw:
+    for widget_and_rect in self.widgets_MenuSystem_to_draw:
       self.game.screen.blit(widget_and_rect[0],widget_and_rect[1])
+    for widget in self.container_widgets_FunnyGUI:
+      widget.draw(self.game.screen)
   
   def confirm_dialog_open_picture_file(self):
     zoom=self.input_box_zoom.text
@@ -133,7 +135,6 @@ class Gui:
     if not self.path_to_picture_to_open:
       return
     self.create_dialog_open_picture_file()
-    self.dialog_open_picture_file.add()
     
   def is_float(self,text):
     try:
