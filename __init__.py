58ac6197e11852ab8c7c68871ecf48d5100dc6f4
František Brožka <sentientfanda@gmail.com>
2019-05-06 12:55:41 +0200

    License this project under the WTFPL License
    Use the license according to http://www.wtfpl.net/faq/ .
    Add a no warranty clause as described in http://www.wtfpl.net/faq/ .
    
    Pick this license because it allows the most freedom, it is public-domain-like license. It was created because the definition of public domain varies with the jurisdictions.

--- __init__.py
+++ __init__.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-    This file is part of Place Pictures.
-    Place Pictures is a program that allows you to play with any of your pictures.
-    Copyright (C) 2017 František Brožka
-    email: sentientfanda@gmail.com
-    website: https://github.com/existenceexists/place_pictures
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# Copyright © 2017 František Brožka <sentientfanda@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
 
 from __future__ import absolute_import
 
