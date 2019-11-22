12cef3518e1f7bd1de8ec7e82cf781c98aae9e01
František Brožka <sentientfanda@gmail.com>
2019-11-22 14:39:36 +0100

    Change internal work with images
    Change internal work with image selected and image highleghted of instance of Picture.
    Do not create image selected and image highlighted and image selected and highlighted
    at the initiation of every Picture instance any more.
    Instead create image selected or image highlighted or image selected and highlighted
    when the event highlight or select occurs.
    Do it to use much less memory then before.

--- picture.py
+++ picture.py
@@ -22,10 +22,9 @@ class Picture(pygame.sprite.Sprite):
     pygame.sprite.Sprite.__init__(self)
     self.path=path
     self.filename=self.path_leaf(path)
-    self.scale=float(scale)
     self.is_highlighted=False
     self.is_selected=False
-    self.create_images()
+    self.scale_image(scale)
     self.rect.center=(center_x,center_y)
     
   def update(self,event):
@@ -36,87 +35,66 @@ class Picture(pygame.sprite.Sprite):
   
   def move_by(self,movement_x,movement_y):
     self.rect.center=(self.rect.center[0]+movement_x,self.rect.center[1]+movement_y)
+  
+  def update_image(self):
+    position=False
+    if hasattr(self,"rect"):
+      position=self.rect.center
+    self.image=self.image_normal
+    self.rect=self.rect_normal
+    if self.is_highlighted and self.is_selected:
+      image_highlighted_and_selected=pygame.Surface((self.rect_normal.width+4,self.rect_normal.height+4)).convert_alpha()
+      rect_highlighted_and_selected=image_highlighted_and_selected.get_rect()
+      image_highlighted_and_selected.fill(pygame.Color(0,0,0,0))
+      pygame.draw.rect(image_highlighted_and_selected,pygame.Color(255,255,255,255),rect_highlighted_and_selected,1)
+      pygame.draw.rect(image_highlighted_and_selected,pygame.Color(255,255,0,255),pygame.Rect(1,1,rect_highlighted_and_selected.width-2,rect_highlighted_and_selected.height-2),1)
+      image_highlighted_and_selected.blit(self.image_normal,(2,2))
+      self.image=image_highlighted_and_selected
+      self.rect=rect_highlighted_and_selected
+    elif self.is_highlighted:
+      image_highlighted=pygame.Surface((self.rect_normal.width+2,self.rect_normal.height+2)).convert_alpha()
+      rect_highlighted=image_highlighted.get_rect()
+      image_highlighted.fill(pygame.Color(0,0,0,0))
+      pygame.draw.rect(image_highlighted,pygame.Color(255,255,255,255),rect_highlighted,1)
+      image_highlighted.blit(self.image_normal,(1,1))
+      self.image=image_highlighted
+      self.rect=rect_highlighted
+    elif self.is_selected:
+      image_selected=pygame.Surface((self.rect_normal.width+2,self.rect_normal.height+2)).convert_alpha()
+      rect_selected=image_selected.get_rect()
+      image_selected.fill(pygame.Color(0,0,0,0))
+      pygame.draw.rect(image_selected,pygame.Color(255,255,0,255),rect_selected,1)
+      image_selected.blit(self.image_normal,(1,1))
+      self.image=image_selected
+      self.rect=rect_selected
+    if not position is False:
+      self.rect.center=position
+  
+  def select(self):
+    self.is_selected=True
+    self.update_image()
+  
+  def unselect(self):
+    self.is_selected=False
+    self.update_image()
     
-  def create_images(self):
+  def highlight(self):
+    self.is_highlighted=True
+    self.update_image()
+  
+  def unhighlight(self):
+    self.is_highlighted=False
+    self.update_image()
+  
+  def scale_image(self,scale):
+    self.scale=float(scale)
     image_unscaled=pygame.image.load(self.path).convert_alpha()
     image_unscaled_rect=image_unscaled.get_rect()
     width=int(round(image_unscaled_rect.width*self.scale))
     height=int(round(image_unscaled_rect.height*self.scale))
     self.image_normal=pygame.transform.scale(image_unscaled,(width,height))
     self.rect_normal=self.image_normal.get_rect()
-    self.image_highlighted=pygame.Surface((self.rect_normal.width+2,self.rect_normal.height+2)).convert_alpha()
-    self.rect_highlighted=self.image_highlighted.get_rect()
-    self.image_highlighted.fill(pygame.Color(0,0,0,0))
-    pygame.draw.rect(self.image_highlighted,pygame.Color(255,255,255,255),self.rect_highlighted,1)
-    self.image_highlighted.blit(self.image_normal,(1,1))
-    self.image_selected=pygame.Surface((self.rect_normal.width+2,self.rect_normal.height+2)).convert_alpha()
-    self.rect_selected=self.image_selected.get_rect()
-    self.image_selected.fill(pygame.Color(0,0,0,0))
-    pygame.draw.rect(self.image_selected,pygame.Color(255,255,0,255),self.rect_selected,1)
-    self.image_selected.blit(self.image_normal,(1,1))
-    self.image_highlighted_and_selected=pygame.Surface((self.rect_normal.width+4,self.rect_normal.height+4)).convert_alpha()
-    self.rect_highlighted_and_selected=self.image_highlighted_and_selected.get_rect()
-    self.image_highlighted_and_selected.fill(pygame.Color(0,0,0,0))
-    pygame.draw.rect(self.image_highlighted_and_selected,pygame.Color(255,255,255,255),self.rect_highlighted_and_selected,1)
-    pygame.draw.rect(self.image_highlighted_and_selected,pygame.Color(255,255,0,255),pygame.Rect(1,1,self.rect_highlighted_and_selected.width-2,self.rect_highlighted_and_selected.height-2),1)
-    self.image_highlighted_and_selected.blit(self.image_normal,(2,2))
-    self.image=self.image_normal
-    self.rect=self.rect_normal
-    if self.is_highlighted and self.is_selected:
-      self.image=self.image_highlighted_and_selected
-      self.rect=self.rect_highlighted_and_selected
-    elif self.is_highlighted:
-      self.image=self.image_highlighted
-      self.rect=self.rect_highlighted
-    elif self.is_selected:
-      self.image=self.image_selected
-      self.rect=self.rect_selected
-  
-  def select(self):
-    self.is_selected=True
-    pos=self.rect.center
-    self.image=self.image_selected
-    self.rect=self.rect_selected
-    if self.is_highlighted:
-      self.image=self.image_highlighted_and_selected
-      self.rect=self.rect_highlighted_and_selected
-    self.rect.center=pos
-  
-  def unselect(self):
-    self.is_selected=False
-    pos=self.rect.center
-    self.image=self.image_normal
-    self.rect=self.rect_normal
-    if self.is_highlighted:
-      self.image=self.image_highlighted
-      self.rect=self.rect_highlighted
-    self.rect.center=pos
-    
-  def highlight(self):
-    self.is_highlighted=True
-    pos=self.rect.center
-    self.image=self.image_highlighted
-    self.rect=self.rect_highlighted
-    if self.is_selected:
-      self.image=self.image_highlighted_and_selected
-      self.rect=self.rect_highlighted_and_selected
-    self.rect.center=pos
-  
-  def unhighlight(self):
-    self.is_highlighted=False
-    pos=self.rect.center
-    self.image=self.image_normal
-    self.rect=self.rect_normal
-    if self.is_selected:
-      self.image=self.image_selected
-      self.rect=self.rect_selected
-    self.rect.center=pos
-  
-  def scale_images(self,scale):
-    self.scale=float(scale)
-    position=self.rect.center
-    self.create_images()
-    self.rect.center=position
+    self.update_image()
     
   def path_leaf(self,path):
     head, tail = ntpath.split(path)
